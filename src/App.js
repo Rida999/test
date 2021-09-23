d5b5540897ee0bb35801e5b5befc13f02d5a101d
rida ajam <ridaajam999@gmail.com>
2021-09-23 21:33:50 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Blogs from "./components/blogs";
+import Left from "./components/leftmenu";
+
+function App() {
+  return (
+    <div>
+      <Left />
+      <Blogs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/leftmenu.js
@@ -0,0 +1,11 @@
+const Left = () => {
+    return ( 
+        <nav>
+            <div className="w-24 h-40 block bg-black">
+
+            </div>
+        </nav>
+     );
+}
+ 
+export default Left;
